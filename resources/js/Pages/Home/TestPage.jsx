f09df7df972f4aee80dc7b178e90dd4305b39ee9
isabellexvr <isabellexavier59@gmail.com>
2025-09-15 20:00:01 -0300

    config: inertia installation

---
+++ resources/js/Pages/Home/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+            <h1>Test React Page</h1>
+            <p>This is a simple test page built with React.</p>
+        </div>
+    );
+};
+
+export default TestPage;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UsersController::class, 'index']);
